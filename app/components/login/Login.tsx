c7419c9432ce4419e1f09d34a2e6f6e3aceab0ce
Harsha Kumar <harzkr142@gmail.com>
2024-03-27 21:15:53 +0530

    Initialise nextjs project with redux

---
+++ app/components/login/Login.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+
+export const Login = () => {
+  return (
+    <div>
+      <p>All login components will come here</p>
+    </div>
+  );
+};
